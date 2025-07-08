8ab3190eff300f78dc8aef355722a90368d6b422
gbixler1977 <gene.bixler@gmail.com>
2025-07-08 14:38:09 -0400

    Update db.js

--- db.js
+++ db.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('donors.db');
 
@@ -52,58 +51,3 @@ module.exports = {
   getCodeByEmail,
   isCodeValid,
 };
-=======
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('donors.db');
-
-// Create table if not exists
-db.serialize(() => {
-  db.run(`
-    CREATE TABLE IF NOT EXISTS donors (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT,
-      email TEXT,
-      code TEXT,
-      timestamp TEXT
-    )
-  `);
-});
-
-function addDonor({ name, email, code }) {
-  const timestamp = new Date().toISOString();
-  return new Promise((resolve, reject) => {
-    db.run(
-      `INSERT INTO donors (name, email, code, timestamp) VALUES (?, ?, ?, ?)`,
-      [name, email, code, timestamp],
-      function (err) {
-        if (err) reject(err);
-        else resolve(this.lastID);
-      }
-    );
-  });
-}
-
-function getCodeByEmail(email) {
-  return new Promise((resolve, reject) => {
-    db.get(`SELECT code FROM donors WHERE email = ? ORDER BY id DESC LIMIT 1`, [email], (err, row) => {
-      if (err) reject(err);
-      else resolve(row?.code || null);
-    });
-  });
-}
-
-function isCodeValid(code) {
-  return new Promise((resolve, reject) => {
-    db.get(`SELECT * FROM donors WHERE code = ?`, [code], (err, row) => {
-      if (err) reject(err);
-      else resolve(!!row);
-    });
-  });
-}
-
-module.exports = {
-  addDonor,
-  getCodeByEmail,
-  isCodeValid,
-};
->>>>>>> 5b6a6d76fe67cc8face9952c8f9f9b1fed833a66
